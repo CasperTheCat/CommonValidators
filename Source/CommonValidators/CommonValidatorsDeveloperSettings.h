d51ea03ddd3675c917ed03e44236a5eac717a6a4
Chris Anderson <11226374+CasperTheCat@users.noreply.github.com>
2025-11-27 21:55:51 +1300

    Adding Categories to allow Engine Pluginisation and moving the ignore lower to catch child assets

--- Source/CommonValidators/CommonValidatorsDeveloperSettings.h
+++ Source/CommonValidators/CommonValidatorsDeveloperSettings.h
@@ -9,11 +9,15 @@ struct FCommonValidatorClassArray
 {
 	GENERATED_BODY()
 	
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Common Validators")
 	TArray<TSubclassOf<UObject>> ClassList;
 
 	// Should this rule propagate to discovered children?
+<<<<<<< HEAD
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+=======
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Common Validators")
+>>>>>>> ecc3dc6 (Adding Categories to allow Engine Pluginisation and moving the ignore lower to catch child assets)
 	bool AllowPropagationToChildren = true;
 };
 
@@ -24,31 +28,31 @@ class COMMONVALIDATORS_API UCommonValidatorsDeveloperSettings : public UDevelope
 
 public:
 	// If true, we will validate for empty tick nodes.
-	UPROPERTY(Config, EditAnywhere)
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators")
 	bool bEnableEmptyTickNodeValidator = true;
 	
 	//If true, we throw an error, otherwise a warning!
-	UPROPERTY(Config, EditAnywhere, meta = (EditCondition = "bEnableEmptyTickNodeValidator == true"))
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators", meta = (EditCondition = "bEnableEmptyTickNodeValidator == true"))
 	bool bErrorOnEmptyTickNodes = true;
 	
 	// if true, we will validate for pure nodes being executed multiple times
-	UPROPERTY(Config, EditAnywhere)
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators")
 	bool bEnablePureNodeMultiExecValidator = true;
 	
 	//If true, we throw an error, otherwise a warning!
-	UPROPERTY(Config, EditAnywhere, meta = (EditCondition = "bEnablePureNodeMultiExecValidator == true"))
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators", meta = (EditCondition = "bEnablePureNodeMultiExecValidator == true"))
 	bool bErrorOnPureNodeMultiExec = true;
 	
 	// If true, we will validate for blocking loads in blueprints
-	UPROPERTY(Config, EditAnywhere)
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators")
 	bool bEnableBlockingLoadValidator = true;
 	
 	//If true, we throw an error, otherwise a warning!
-	UPROPERTY(Config, EditAnywhere, meta = (EditCondition = "bEnableBlockingLoadValidator == true"))
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators", meta = (EditCondition = "bEnableBlockingLoadValidator == true"))
 	bool bErrorBlockingLoad = true;
 
 	// If true, we will validate for references above the set value in blueprints
-	UPROPERTY(Config, EditAnywhere)
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators")
 	bool bEnableHeavyReferenceValidator = true;
 
 	// If the total size on disk is above this, we consider the asset heavy
@@ -56,14 +60,18 @@ public:
 	int MaximumAllowedReferenceSizeKiloBytes = 20480;
 
 	//If true, we throw an error, otherwise a warning!
-	UPROPERTY(Config, EditAnywhere, meta = (EditCondition = "bEnableHeavyReferenceValidator == true"))
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators", meta = (EditCondition = "bEnableHeavyReferenceValidator == true"))
+	int MaximumAllowedReferenceSizeKiloBytes = 128;
+
+	//If true, we throw an error, otherwise a warning!
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators", meta = (EditCondition = "bEnableHeavyReferenceValidator == true"))
 	bool bErrorHeavyReference = false;
 
 	// Classes in this list, and their children, are ignored by heavy reference validator
-	UPROPERTY(Config, EditAnywhere, meta = (EditCondition = "bEnableHeavyReferenceValidator == true"))
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators", meta = (EditCondition = "bEnableHeavyReferenceValidator == true"))
 	TArray<TSubclassOf<UObject>> HeavyValidatorClassAndChildIgnoreList = {UAnimBlueprint::StaticClass()};
 
 	// Classes in this list, and only classes in this list, are ignored by heavy reference validator
-	UPROPERTY(Config, EditAnywhere, meta = (EditCondition = "bEnableHeavyReferenceValidator == true"))
+	UPROPERTY(Config, EditAnywhere, Category="Common Validators", meta = (EditCondition = "bEnableHeavyReferenceValidator == true"))
 	TMap<TSubclassOf<UObject>, FCommonValidatorClassArray> HeavyValidatorClassSpecificClassIgnoreList;
 };
